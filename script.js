b45fe044ef7bb0fa09a9c6ae0c9a3bff70f90031
Fikri Roslee <fikriroslee16@gmail.com>
2023-01-10 23:36:12 +0800

    Added highscores and refactored some code in script.js

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
 "use strict";
 
-/* 
-// Getting the content of a selected element using querySelector and textContent
-console.log(document.querySelector(".message").textContent);
-
-// Changing an existing text content
-// This is an example of DOM manipulation
-document.querySelector(".message").textContent = "🎉 Correct Number!";
-console.log(document.querySelector(".message").textContent);
-
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-
-// Setting the value for an input field using the ".value" property
-document.querySelector(".guess").value = 23;
-console.log(document.querySelector(".guess").value); 
-*/
-
 // Defining a secret number
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 // Defining a score for the game.
 // This variable can also be called a "state variable".
@@ -27,36 +9,60 @@ document.querySelector(".number").textContent = secretNumber;
 // meaning that data that is relevant to the application.
 let score = 20;
 
+// Defining a highscore variable
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 ///// Handling Click Events /////
 // 1. Selected an element using the querySelector.
 // 2. Used an addEventListener method on that element to attach an event handler.
 // 3. The event handler is a function expression, therefore, we can use the function as a parameter
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
 
   // Implementing the game logic
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔ No number! ";
+    displayMessage("⛔ No number! ");
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "🎉 Correct number!";
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "📈 Too high!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "💥 You lost the game!";
-      document.querySelector(".score").textContent = 0;
+    document.querySelector(".number").textContent = secretNumber;
+    displayMessage("🎉 Correct number!");
+
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
     }
-  } else if (guess < secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "📉 Too low!";
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "💥 You lost the game!";
+      displayMessage("💥 You lost the game!");
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+// An event handler to reset the game back to its original state
+document.querySelector(".again").addEventListener("click", function () {
+  // Restoring the score to its initial value
+  // and initialising a new random number for the secretNumber variable
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // Reset text
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = score;
+  displayMessage("Start guessing...");
+
+  // Reset styles
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
 });
